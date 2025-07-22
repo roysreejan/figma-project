3ff3f161893b6627d02f42451f858c30e4ad8da3
roysreejan <sreejanroy01@gmail.com>
2025-07-22 13:23:12 +0600

    navbar and footer added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import { Inter } from "next/font/google";
+import Link from "next/link";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const Navbar = () => {
+  const navItems = [
+    {
+      name: "Works",
+      link: "#",
+    },
+    {
+      name: "Blog",
+      link: "#",
+    },
+    {
+      name: "Contact",
+      link: "#",
+    },
+  ];
+  return (
+    <ul
+      className={`${inter.className} m-[66px] flex items-center justify-end h-[24px] font-medium text-[20px] space-x-6`}
+    >
+      {navItems.map((item, i) => (
+        <li key={i}>
+          <Link href={item.link}>{item.name}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Navbar;
